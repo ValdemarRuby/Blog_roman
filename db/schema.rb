377611208820e7664bf46f9becfb3345d569e1cf
ValdemarRuby <wings6928@gmail.com>
2019-08-16 18:38:48 +0300

    Add new contact form

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_111532) do
+ActiveRecord::Schema.define(version: 2019_08_16_151621) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "mail"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
