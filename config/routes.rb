c858d0ba381372b5d07e4e6db298a590f62b6879
ValdemarRuby <wings6928@gmail.com>
2019-08-21 12:10:25 +0300

    Add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#index"
+
   get '/' => 'home#index'
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
