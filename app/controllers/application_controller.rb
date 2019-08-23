f54b3c03eadd6ec209679c250de905b96dfac03e
ValdemarRuby <wings6928@gmail.com>
2019-08-23 13:43:50 +0300

    Fixed error with write username in db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # при переходе через devise_controller будет выполнен следующий фильтр
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-  #
-  # private
-  #
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, :kyes [:username])
-  # end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
